0fd9575871e1dfa00d4ee25bd00a979322c0f7c3
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2013-08-05 12:21:59 +0700

    lazy load on click

--- application/views/index.php
+++ application/views/index.php
@@ -3,14 +3,12 @@ Ext.Loader.setPath('Account', __base_url+'assets/js');
 
 Ext.onReady(function() {
 
-	$om.prDialog = Ext.create('Account.PR.MainWindow');
-
-	$om.warehouseDialog = Ext.create('Account.Warehouse.MainWindow');
-
 	$om.viewport.on('click_income', function(){
+		$om.prDialog = Ext.create('Account.PR.MainWindow');
 		$om.prDialog.show();
 	});
 	$om.viewport.on('click_journal', function(){
+		$om.warehouseDialog = Ext.create('Account.Warehouse.MainWindow');
 		$om.warehouseDialog.show();
 	});
 
