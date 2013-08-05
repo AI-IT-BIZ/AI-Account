8e1aa2ffe6477092f3f75996934f5855e6923543
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2013-08-05 12:27:40 +0700

    check initial one time

--- application/views/index.php
+++ application/views/index.php
@@ -4,11 +4,13 @@ Ext.Loader.setPath('Account', __base_url+'assets/js');
 Ext.onReady(function() {
 
 	$om.viewport.on('click_income', function(){
-		$om.prDialog = Ext.create('Account.PR.MainWindow');
+		if(!$om.prDialog)
+			$om.prDialog = Ext.create('Account.PR.MainWindow');
 		$om.prDialog.show();
 	});
 	$om.viewport.on('click_journal', function(){
-		$om.warehouseDialog = Ext.create('Account.Warehouse.MainWindow');
+		if(!$om.warehouseDialog)
+			$om.warehouseDialog = Ext.create('Account.Warehouse.MainWindow');
 		$om.warehouseDialog.show();
 	});
 
