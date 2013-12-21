9eb4741fc1f70b9c0edc6577bfda4655911d28c4
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2013-12-21 18:31:11 +0700

    add email service

---
+++ application/config/email.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+
+$config['useragent']        = 'PHPMailer';              // Mail engine switcher: 'CodeIgniter' or 'PHPMailer'
+$config['protocol']         = 'smtp';                   // 'mail', 'sendmail', or 'smtp'
+$config['mailpath']         = '/usr/sbin/sendmail';
+$config['smtp_host']        = 'mail.primebiznets.com';
+$config['smtp_user']        = 'kitipong@primebiznets.com';
+$config['smtp_pass']        = 'prime1234';
+$config['smtp_port']        = 25;
+$config['smtp_timeout']     = 5;                        // (in seconds)
+$config['smtp_crypto']      = '';                       // '' or 'tls' or 'ssl'
+$config['wordwrap']         = true;
+$config['wrapchars']        = 76;
+$config['mailtype']         = 'html';                   // 'text' or 'html'
+$config['charset']          = 'utf-8';
+$config['validate']         = true;
+$config['priority']         = 3;                        // 1, 2, 3, 4, 5
+$config['crlf']             = "\n";                     // "\r\n" or "\n" or "\r"
+$config['newline']          = "\n";                     // "\r\n" or "\n" or "\r"
+$config['bcc_batch_mode']   = false;
+$config['bcc_batch_size']   = 200;
