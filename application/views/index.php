f453abbf8db0c18710707754b5cfc42f4cdb9eee
unknown <aon@.(none)>
2014-01-02 17:39:46 +0700

    add link Rpnd50WHT (aon)

--- application/views/index.php
+++ application/views/index.php
@@ -378,28 +378,33 @@ Ext.onReady(function() {
 	});
 	
 	$om.viewport.on('click_RSumVat', function(){
-		$om.RGeneralJournal = Ext.create('Account.RSumVat.MainWindow');
-		$om.RGeneralJournal.show();
+		$om.RSumVat = Ext.create('Account.RSumVat.MainWindow');
+		$om.RSumVat.show();
 	});
 	
 	$om.viewport.on('click_Rpp30Vat', function(){
-		$om.RGeneralJournal = Ext.create('Account.Rpp30Vat.MainWindow');
-		$om.RGeneralJournal.show();
+		$om.Rpp30Vat = Ext.create('Account.Rpp30Vat.MainWindow');
+		$om.Rpp30Vat.show();
 	});
 	
 	$om.viewport.on('click_Rpnd1WHT', function(){
-		$om.RGeneralJournal = Ext.create('Account.Rpnd1WHT.MainWindow');
-		$om.RGeneralJournal.show();
+		$om.Rpnd1WHT = Ext.create('Account.Rpnd1WHT.MainWindow');
+		$om.Rpnd1WHT.show();
 	});
 
 	$om.viewport.on('click_Rpnd3WHT', function(){
-		$om.RGeneralJournal = Ext.create('Account.Rpnd3WHT.MainWindow');
-		$om.RGeneralJournal.show();
+		$om.Rpnd3WHT = Ext.create('Account.Rpnd3WHT.MainWindow');
+		$om.Rpnd3WHT.show();
+	});
+	
+	$om.viewport.on('click_Rpnd50WHT', function(){
+		$om.Rpnd50WHT = Ext.create('Account.Rpnd50WHT.MainWindow');
+		$om.Rpnd50WHT.show();
 	});
 	
 	$om.viewport.on('click_Rpnd53WHT', function(){
-		$om.RGeneralJournal = Ext.create('Account.Rpnd53WHT.MainWindow');
-		$om.RGeneralJournal.show();
+		$om.Rpnd53WHT = Ext.create('Account.Rpnd53WHT.MainWindow');
+		$om.Rpnd53WHT.show();
 	});
 });
 </script>
