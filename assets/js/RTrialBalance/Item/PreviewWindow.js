25b2667b18aa0060631fe8616f548d821f817134
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2014-01-05 23:20:55 +0700

    update preview window

--- assets/js/RTrialBalance/Item/PreviewWindow.js
+++ assets/js/RTrialBalance/Item/PreviewWindow.js
@@ -1,84 +1,18 @@
 Ext.define('Account.RTrialBalance.Item.PreviewWindow', {
-	extend	: 'Ext.window.Window',
+	extend	: 'BASE.PreviewWindow',
 	constructor:function(config) {
+		var _this=this;
 
 		Ext.apply(this, {
 			title: 'Trial Balance preview',
-			closeAction: 'hide',
-			height: 600,
-			width: 830,
-			minHeight: 600,
-			minWidth: 830,
-			layout: 'border',
-			border: false,
-			resizable: true,
-			modal: true,
-			buttonAlign:'center'
+
+			enableCopies: false
 		});
 
 		return this.callParent(arguments);
 	},
 	initComponent : function() {
-		var _this=this;
-
-		this.items = [new Ext.Panel({
-			region:'center',
-			border: true,
-			items:[{
-				xtype: 'component',
-				id: 'preview_frame',
-		        autoEl: {
-					tag: 'iframe',
-					src: 'about:blank',
-					frameBorder: 0,
-					width: '100%',
-					height: '100%',
-					style: {
-						overflow: 'auto'
-					}
-		     	}
-			}]
-        })];
-
-		this.on('hide', function(){
-			Ext.get('preview_frame').set({
-				src:'about:blank'
-			});
-		});
-
 
 		return this.callParent(arguments);
-	},
-	dialogId: null,
-	openDialog:function(url){
-		var _this=this;
-		if(url){
-			this.show(false, function(){
-				Ext.get('preview_frame').set({
-					src:url
-				});
-				_this.showFrameLoad();
-				_this.checkFrameReady(function(){
-					_this.hideFrameLoad();
-				});
-			});
-		}
-	},
-	
-	checkFrameReady: function(cb){
-		document.getElementById('preview_frame').onload = cb;
-	},
-	showFrameLoad: function(){
-        Ext.MessageBox.show({
-           msg: 'Populating preview...',
-           progressText: 'Loading...',
-           width:300,
-           wait:true,
-           waitConfig: {interval:200},
-           animateTarget: this
-       });
-	},
-	hideFrameLoad: function(){
-		Ext.MessageBox.hide();
 	}
 });
