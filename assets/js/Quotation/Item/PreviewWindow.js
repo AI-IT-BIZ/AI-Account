c682e1a3140617837ad50a329eff219aecf075bd
unknown <aon@.(none)>
2014-03-04 21:35:12 +0700

    merg file (aon)

--- assets/js/Quotation/Item/PreviewWindow.js
+++ assets/js/Quotation/Item/PreviewWindow.js
@@ -1,11 +1,10 @@
 Ext.define('Account.Quotation.Item.PreviewWindow', {
 	extend	: 'BASE.PreviewWindow',
 	constructor:function(config) {
-		var _this=this;
 
 		Ext.apply(this, {
 			title: 'Quotation preview',
-			enableCopies: false
+			enableCopies: true
 		});
 
 		return this.callParent(arguments);
@@ -14,10 +13,10 @@ Ext.define('Account.Quotation.Item.PreviewWindow', {
 
 		return this.callParent(arguments);
 	},
-	/*
+	
 	getFrameUrl: function(id, copies){
 		copies = copies || 1;
-		return __site_url+'formbof/quotation/index/'+id+'/'+copies;
-	}*/
+		return __site_url+'form/quotation/index/'+id+'/'+copies;
+	}
 
 });
